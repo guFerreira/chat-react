42fe0d0580479c9b1348b5c32446746c153cddde
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-07-20 19:28:23 -0300

    adicionando configuracao de rotas

--- src/components/Chat/components/ChatBody/index.tsx
+++ src/components/Chat/components/ChatBody/index.tsx
@@ -1,7 +1,20 @@
 
 function ChatBody() {
+
+  const lista = [{mensagem:'olá, eu sou o xaropinho', horario:'6 e onibus', linkAudio: '<iframe width="110" height="200" src="https://www.myinstants.com/instant/ele-gosta-1026/embed/" frameborder="0" scrolling="no"></iframe>'},
+  {mensagem:'olá, eu sou o xaropinho', horario:'6 e onibus', linkAudio: '<iframe width="110" height="200" src="https://www.myinstants.com/instant/ele-gosta-1026/embed/" frameborder="0" scrolling="no"></iframe>'},
+  {mensagem:'olá, eu sou o xaropinho', horario:'6 e onibus', linkAudio: '<iframe width="110" height="200" src="https://www.myinstants.com/instant/ele-gosta-1026/embed/" frameborder="0" scrolling="no"></iframe>'}  
+]
+
     return (
-      <div className="App">
+      <div className="App" >
+        {lista.map((elemento) => {
+          return <div>
+            <p style={{backgroundColor:'blue', padding:'10px'}}>mensagem:{elemento.mensagem}</p>
+            <p style={{backgroundColor:'blue', padding:'10px'}}>horario:{elemento.horario}</p>
+            <div dangerouslySetInnerHTML={{ __html: elemento.linkAudio }} />
+            </div>
+        })}
         <p>body</p>
       </div>
     );
