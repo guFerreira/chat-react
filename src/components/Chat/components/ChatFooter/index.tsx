e978257a8de006e1fbdb2fd473fd1228f6c9e36d
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-08-16 23:05:26 -0300

    exibir mensagem enviada na tela

--- src/components/Chat/components/ChatFooter/index.tsx
+++ src/components/Chat/components/ChatFooter/index.tsx
@@ -2,24 +2,44 @@ import SendIcon from '@mui/icons-material/Send';
 import './chat-footer.css';  
 import Message from '../ChatBody/components/message';
 import { useAtom } from "jotai";
-import { messagesAtom } from "../../../../atoms";
+import { messagesAtom, newMessageAtom } from "../../../../atoms";
 
 function ChatFooter() {
   const [messages, setMessages] = useAtom(messagesAtom);
+  const [newMessage, setNewMessage] = useAtom(newMessageAtom);
 
-  function sendMessage() {
-    
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => setNewMessage(e.target.value)
+
+  const resetInput = () =>{
+    setNewMessage('')
+  }
+
+  function sendMessage(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const buildedNewMessage:Message = {
+      text: newMessage,
+      received:false,
+      audio:'',
+      image:''
+    }
+    setMessages([...messages, buildedNewMessage])
+    resetInput()
   }
 
     return (
       <div className="App">
-        <div className="footer-chat">
-          <input className="text-input" type="text" placeholder="Digite uma mensagem.." />
-            <button className="send-button" onClick={sendMessage}>
+        <form className="footer-chat" onSubmit={sendMessage}>
+          <input 
+            className="text-input"
+            type="text" placeholder="Digite uma mensagem.."
+            onChange={handleChange}
+            value={newMessage}
+          />
+            <button className="send-button" type="submit">
                 <SendIcon/>
                 <span className="enviar-texto">Enviar</span>
             </button>
-        </div>
+        </form>
       </div>
     );
   }
